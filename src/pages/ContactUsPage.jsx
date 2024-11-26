a2e0714dc7d65298ca955452f33a7e32d38f9113
travisdleslie <hello@travisleslie.com>
2024-11-26 13:16:15 -0700

    remove contact us

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const ContactUsPage = () => {
   return (
     <section className="bg-[#0F0F0F] flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">Contact Us</h1>
-    </section>
+   </section>
   );
 };
 
